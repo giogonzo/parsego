69d8b40ec09f2a20fee58a97eba966a3df533d72
Giovanni Gonzaga <gio@webdevelovers.com>
2013-01-19 10:44:43 -0600

    initial import

---
+++ parse.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"parsego/parser"
+)
+
+/*
+	Matches an IDENTIFIER
+*/
+func Identifier() pg.Parser {
+	return pg.Concat(pg.Char(), pg.Many(pg.Any(pg.Try(pg.Char()), pg.Try(pg.Number()))))
+}
+
+/*
+	Matches a NUMBER_LITERAL
+*/
+func NumberLiteral() pg.Parser {
+	return pg.Many1(pg.Number())
+}
+
+/*
+	Matches a STRING_LITERAL
+*/
+func StringLiteral() pg.Parser {
+	return pg.Concat(pg.Character('"'), pg.Many(pg.Any(pg.Try(pg.Char()), pg.Try(pg.Number()))), pg.Character('"'))
+}
+
+/*
+	Matches a BOOL_LITERAL
+*/
+func BoolLiteral() pg.Parser {
+	return pg.Any(pg.Try(pg.String("true")), pg.Try(pg.String("false")))
+}
+
+/*
+	Matches a LITERAL
+*/
+func Literal() pg.Parser {
+	return pg.Any(pg.Try(NumberLiteral()), pg.Try(StringLiteral()), pg.Try(BoolLiteral()))
+}
+
+/*
+	Matches an ASSIGNMENT
+*/
+func Assignment() pg.Parser {
+	return pg.Trim(pg.Concat(Identifier(), pg.Whitespaces(), pg.Equal(), pg.Whitespaces(), Expression()))
+}
+
+/*
+	TODO
+	Matches an EXPRESSION
+*/
+func Expression() pg.Parser {
+	return pg.Any(pg.Try(Literal()), pg.Try(Identifier()))
+}
+
+/*
+
+*/
+func main() {
+	in := new(pg.StringState)
+
+	// in.SetInput("aabaa")
+	// out, ok := Many(Any(Try(Character('a')), Try(Character('b'))))(in)
+
+	// in.SetInput("bella")
+	// out, ok := Many(Char())(in)
+
+	// in.SetInput("var1")
+	// out, ok := Identifier()(in)
+
+	// in.SetInput("varName =		34")
+	// out, ok := Assignment()(in)
+
+	in.SetInput(`
+		varName = true
+		test=	10
+		`)
+	out, ok := pg.Many(Assignment())(in)
+
+	fmt.Printf("-------\n")
+	fmt.Printf("Parse ok: %b\n", ok)
+	fmt.Printf("Last position: %d\n", in.GetPosition())
+	fmt.Printf("Parsed: -%s-\n", out)
+}
---
+++ src/parsego/parser/parser.go
@@ -0,0 +1,304 @@
+package pg
+
+import (
+	"fmt"
+	"regexp"
+)
+
+type State interface {
+	Next() (int, bool)
+	SetInput(in string)
+	GetPosition() int
+	SetPosition(position int)
+}
+
+type Parser func(in State) (Output, bool)
+
+type Output []byte
+
+/*
+
+*/
+
+type StringState struct {
+	position int
+	input    []byte
+}
+
+func (self *StringState) Next() (int, bool) {
+	if self.position >= len(self.input) {
+		return 0, false
+	}
+	self.position += 1
+	return int(self.input[self.position-1]), true
+}
+
+func (self *StringState) SetInput(in string) {
+	self.input = []byte(in)
+}
+
+func (self *StringState) GetPosition() int {
+	return self.position
+}
+
+func (self *StringState) SetPosition(position int) {
+	self.position = position
+}
+
+/*
+
+*/
+
+/*
+	Matches a single character
+*/
+func Character(c int) Parser {
+	return func(in State) (Output, bool) {
+		target, ok := in.Next()
+		if ok && c == int(target) {
+			return []byte{byte(c)}, true
+		}
+		return nil, false
+	}
+}
+
+/*
+	Matches *
+*/
+func Many(match Parser) Parser {
+	return func(in State) (Output, bool) {
+		matched := make([]byte, 0)
+		for {
+			out, ok := match(in)
+			if !ok {
+				break
+			}
+
+			if out != nil {
+				matched = concat(matched, out)
+			}
+		}
+		return matched, true
+	}
+}
+
+/*
+	Matches {n,n}
+*/
+func ManyN(match Parser, n int) Parser {
+	return func(in State) (Output, bool) {
+		matched := make([]byte, 0)
+		for i := 0; i < n; i += 1 {
+			out, ok := match(in)
+			if !ok {
+				return nil, false
+			}
+			fmt.Printf("%d: %s\n", i, out)
+
+			if out != nil {
+				matched = concat(matched, out)
+			}
+		}
+		return matched, true
+	}
+}
+
+/*
+	TODO: check if this works
+	Matches +
+*/
+func Many1(match Parser) Parser {
+	return func(in State) (Output, bool) {
+		out, ok := match(in)
+		if !ok {
+			return nil, false
+		}
+		matched := []byte(out)
+
+		for {
+			out, ok := match(in)
+			if !ok {
+				break
+			}
+
+			if out != nil {
+				matched = concat(matched, out)
+			}
+		}
+		return matched, true
+	}
+}
+
+/*
+	Matches parsers in |
+	Wrap parsers in Try(...) calls to preserve state
+*/
+func Any(matches ...Parser) Parser {
+	return func(in State) (Output, bool) {
+		for _, match := range matches {
+			out, ok := match(in)
+			if ok {
+				return out, true
+			}
+			fmt.Printf("Any failed\n")
+		}
+		return nil, false
+	}
+}
+
+/*
+	Tries to match, preserving initial state in case of fail
+*/
+func Try(match Parser) Parser {
+	return func(in State) (Output, bool) {
+		initialPosition := in.GetPosition()
+		out, ok := match(in)
+		if !ok {
+			in.SetPosition(initialPosition)
+			fmt.Printf("Try failed\n")
+		}
+		return out, ok
+	}
+}
+
+/*
+	Matches [a-zA-Z]
+*/
+func Char() Parser {
+	return func(in State) (Output, bool) {
+		target, ok := in.Next()
+		if ok {
+			match, _ := regexp.Match("[a-zA-Z]", []byte{byte(target)})
+			if match {
+				return []byte{byte(target)}, true
+			}
+		}
+		return nil, false
+	}
+}
+
+/*
+	Matches [0-9]
+*/
+func Number() Parser {
+	return func(in State) (Output, bool) {
+		target, ok := in.Next()
+		if ok {
+			match, _ := regexp.Match("[0-9]", []byte{byte(target)})
+			if match {
+				return []byte{byte(target)}, true
+			}
+		}
+		return nil, false
+	}
+}
+
+/*
+	Matches concatenation
+*/
+func Concat(matches ...Parser) Parser {
+	return func(in State) (Output, bool) {
+		matched := make([]byte, 0)
+		for _, match := range matches {
+			fmt.Printf("Concat start pos.: %d\n", in.GetPosition())
+			out, ok := match(in)
+			if !ok {
+				return matched, false
+			}
+			matched = concat(matched, out)
+			fmt.Printf("Concat: -%s-\n", matched)
+		}
+		return matched, true
+	}
+}
+
+/*
+	Matches [\s]
+*/
+func Whitespace() Parser {
+	return func(in State) (Output, bool) {
+		target, ok := in.Next()
+		if ok {
+			match, _ := regexp.Match("\\s", []byte{byte(target)})
+			if match {
+				return []byte{byte(target)}, true
+			}
+		}
+		return nil, false
+	}
+}
+
+/*
+	Skips matching
+*/
+func Skip(match Parser) Parser {
+	return func(in State) (Output, bool) {
+		out, ok := match(in)
+		fmt.Printf("Skipping: -%s-\n", out)
+		if ok {
+			in.SetPosition(in.GetPosition() - 1)
+		}
+		return nil, ok
+	}
+}
+
+/*
+	Skips whitespace(s)
+*/
+func Whitespaces() Parser {
+	return Skip(Many(Whitespace()))
+}
+
+/*
+	Trims matching
+*/
+func Trim(match Parser) Parser {
+	return func(in State) (Output, bool) {
+		Whitespaces()(in)
+		out, ok := match(in)
+		Whitespaces()(in)
+		return out, ok
+	}
+}
+
+/*
+	Matches =
+*/
+func Equal() Parser {
+	return Character('=')
+}
+
+/*
+	Matches ;
+*/
+func Semi() Parser {
+	return Character(';')
+}
+
+/*
+	Matches exact string
+*/
+func String(s string) Parser {
+	return func(in State) (Output, bool) {
+		matched := make([]byte, 0)
+		for _, c := range s {
+			out, ok := Character(int(c))(in)
+			if !ok {
+				return matched, false
+			}
+			matched = append(matched, out[0])
+		}
+		return matched, true
+	}
+}
+
+/*
+	Utility
+*/
+
+func concat(old1, old2 []byte) []byte {
+	newslice := make([]byte, len(old1)+len(old2))
+	copy(newslice, old1)
+	copy(newslice[len(old1):], old2)
+	return newslice
+}
